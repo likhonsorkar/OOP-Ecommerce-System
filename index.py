4cece64a2c2b39c85fca8b5f1da350dd3d8e8241
unknown <likhonsorkar002@gmail.com>
2026-05-04 15:51:36 +0600

    some oop implement

--- index.py
+++ index.py
@@ -2,8 +2,16 @@ ALL_PRODUCTS = []
 class Product:
     def __init__(self, name, price):
         self.name = name
-        self.price = price
+        self.__price = price #Encapsulation 
         ALL_PRODUCTS.append(self)
+    @property
+    def price(self):
+        return self.__price
+#inheritance
+class WarrantyProduct(Product):
+    def __init__(self, name, price, warranty):
+        super().__init__(name, price)
+        self.warranty = warranty
 class Cart:
     def __init__(self):
         self.items = {}
@@ -17,30 +25,32 @@ class Cart:
         if not self.items:
             print("\nYour cart is empty!")
             return
-        print("\n ============== YOUR CART ==============")
-        print("SL - Product Name - Price - Quentity -Net  Price ")
+        print("\n ================== YOUR CART ====================")
+        print("SL. Name(Warranty)\t Price \tQTY\tNetPrice ")
         total = 0
         counter = 1
         for product in self.items:
+            warranty_info = f" (YES)" if hasattr(product, 'warranty') else "NO"
             qty = self.items[product]
             net = product.price * qty
             total += net
-            print(f"{counter}. {product.name} - {product.price} - {qty} - {net}")
+            print(f"{counter}.{product.name}{warranty_info}\t \t{product.price}\t{qty} \t{net}")
             counter += 1
-        print("------------------------------------------")
-        print(f"Total: {total} TK")
+        print("---------------------------------------------------")
+        print(f"\t \t \t \tTotal: {total} TK")
 
 def show_all_products():
     print("\n --------------- All Products List -----------------")
-    print(f"SL NO. Product Name - Price ")
+    print(f"SL. Product Name - Price ")
     counter = 1
     for product in ALL_PRODUCTS:
-        print(f"{counter}. {product.name} - {product.price} TK ")
+        warranty_info = f" ({product.warranty}y Warranty)" if hasattr(product, 'warranty') else ""
+        print(f"{counter}. {product.name} - {product.price} TK {warranty_info}")
         counter += 1
     print("\n --------------- End Products List -----------------")
 
 p1 = Product("Laptop", 50000)
-p2 = Product("Mouse", 1200)
+p2 = WarrantyProduct("Mouse", 1200 , 1)
 p3 = Product("Keyboard", 2500)
 my_cart = Cart()
 
@@ -62,7 +72,7 @@ while True:
             else:
                 print("Invalid product number!")
         except ValueError:
-            print("❌ Please enter a valid number!")
+            print("Please enter a valid number!")
     elif choice == '2':
         my_cart.show_cart()
 
